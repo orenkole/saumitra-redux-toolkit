245c6bbebebcf188b36eecbce2596e80aab91bb5
badger <orenkole@gmail.com>
2022-07-31 12:55:51 +0300

    Section 5. Working on form

--- rtk-crud/src/pages/AddEdit.tsx
+++ rtk-crud/src/pages/AddEdit.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useNavigate} from "react-router-dom";
+
+const initialState = {
+    name: '',
+    email: '',
+    contact: ''
+}
+
+const handleInputChange = (e) => {
+
+}
+
+const handleSubmit = () => {
+
+}
 
 const AddEdit = () => {
+    const [formValue, setFormValue] = useState(initialState);
+    const {name, email, contact} = formValue;
+    const navigate = useNavigate();
+
     return (
-        <div>
-            <h2>Add edit</h2>
+        <div style={{ marginTop: '100px' }}>
+            <form style={{ margin: 'auto', padding: '15px', maxWidth: '400px', alignContent: 'center' }} onSubmit={handleSubmit}>
+                <label htmlFor="name">Name</label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="Enter name ..."
+                    value={name}
+                    onChange={handleInputChange}
+                />
+                <label htmlFor="email">Email</label>
+                <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    placeholder="Enter email ..."
+                    value={email}
+                    onChange={handleInputChange}
+                />
+                <label htmlFor="contact">Contact</label>
+                <input
+                    type="number"
+                    id="contact"
+                    name="contact"
+                    placeholder="Enter contact number ..."
+                    value={contact}
+                    onChange={handleInputChange}
+                />
+                <input type="submit" value="Add" />
+            </form>
         </div>
     );
 };
